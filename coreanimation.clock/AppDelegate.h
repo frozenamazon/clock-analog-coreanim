2339c2c3fde51a560ba3d63e65c93292ff3d0945
Lisa Lau <lisa@lisas-mbp>
2017-04-11 16:21:02 +0100

    Initial Commit

---
+++ coreanimation.clock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreanimation.clock
+//
+//  Created by Lisa Lau on 11/04/2017.
+//  Copyright © 2017 Lisa Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coreanimation.clock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coreanimation.clock
+//
+//  Created by Lisa Lau on 11/04/2017.
+//  Copyright © 2017 Lisa Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
