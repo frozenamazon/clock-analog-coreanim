fcbeabb1ae6c7cb3e5a49212b9426925942ebe68
Lisa Lau <lisa@lisas-mbp.home>
2017-05-09 17:22:06 +0100

    Update clock UI

--- coreanimation.clock/ViewController.h
+++ coreanimation.clock/ViewController.h
@@ -2,8 +2,6 @@
 //  ViewController.h
 //  coreanimation.clock
 //
-//  Created by Lisa Lau on 11/04/2017.
-//  Copyright © 2017 Lisa Lau. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- coreanimation.clock/ViewController.m
+++ coreanimation.clock/ViewController.m
@@ -2,9 +2,6 @@
 //  ViewController.m
 //  coreanimation.clock
 //
-//  Created by Lisa Lau on 11/04/2017.
-//  Copyright © 2017 Lisa Lau. All rights reserved.
-//
 
 #import "ViewController.h"
 
@@ -19,22 +16,26 @@
 
 @implementation ViewController
 
--(CALayer*) createSecondsForClockFrame:(CGFloat)size {
+-(CALayer*) createTicksForClockFrame:(CGFloat)size {
     
     CAShapeLayer* clockFrame = [CAShapeLayer layer];
-    [clockFrame setPath:[[UIBezierPath bezierPathWithOvalInRect:CGRectMake(0,0, size, size)] CGPath]];
-    [clockFrame setStrokeColor:[[UIColor blackColor] CGColor]];
-    clockFrame.lineWidth = 8;
-    [clockFrame setFillColor:[[UIColor clearColor] CGColor]];
+//    [clockFrame setPath:[[UIBezierPath bezierPathWithOvalInRect:CGRectMake(0,0, size, size)] CGPath]];
+//    [clockFrame setStrokeColor:[[UIColor blackColor] CGColor]];
+//    clockFrame.lineWidth = 8;
+//    [clockFrame setFillColor:[[UIColor clearColor] CGColor]];
     
-    for (int i = 0; i < 60; i ++) {
+    CAShapeLayer* dot = [CAShapeLayer layer];
+    dot.position = CGPointMake(size/2 - 3, size/2 - 3);
+    [dot setPath:[[UIBezierPath bezierPathWithOvalInRect:CGRectMake(0,0, 6, 6)] CGPath]];
+    [dot setFillColor:[[UIColor blackColor] CGColor]];
+    [clockFrame addSublayer:dot];
+    
+    //Add the second ticks
+    for (int i = 0; i < 180; i ++) {
         
         double secondsLength = 10;
         double secondsWidth = 2;
-        
-        if(i%5 == 0) {
-            secondsLength = 20;
-        }
+        double degreeForOneThirdSecond = 2.0; //there is a line for every 2 degrees
         
         CALayer* innerClockFrame = [CALayer layer];
         innerClockFrame.backgroundColor = [[UIColor clearColor] CGColor];
@@ -42,17 +43,37 @@
         innerClockFrame.position = CGPointMake(size/2, size/2);
         innerClockFrame.bounds = CGRectMake(0, 0, secondsWidth, size/2);
         
-        CALayer* innerClockFrameSeconds = [CALayer layer];
-        innerClockFrameSeconds.backgroundColor = [[UIColor blackColor] CGColor];
-        innerClockFrameSeconds.anchorPoint = CGPointMake(0, 0);
-        innerClockFrameSeconds.position = CGPointMake(0 ,size/2 - secondsLength);
-        innerClockFrameSeconds.bounds = CGRectMake(0, 0, secondsWidth, secondsLength);
+        if( i % 3 == 0) {
+            secondsLength = 1.5 * secondsLength;
+        }
         
-        [innerClockFrame addSublayer:innerClockFrameSeconds];
-        innerClockFrame.transform = CATransform3DMakeRotation(-i * (6.0/180 *M_PI), 0, 0, 1);
+        if( i % 15 == 0) {
+            
+            CALayer* innerClockFrameHourIndicator = [CALayer layer];
+            innerClockFrameHourIndicator.backgroundColor = [[UIColor grayColor] CGColor];
+            innerClockFrameHourIndicator.anchorPoint = CGPointMake(0, 0);
+            innerClockFrameHourIndicator.position = CGPointMake(0 ,0.6 * size/2);
+            innerClockFrameHourIndicator.cornerRadius = 2;
+            innerClockFrameHourIndicator.bounds = CGRectMake(0, 0, 2 * secondsWidth, 0.2 * size/2);
+            
+            [innerClockFrame addSublayer:innerClockFrameHourIndicator];
+        }
+        
+        CALayer* innerClockFrameTicks = [CALayer layer];
+        innerClockFrameTicks.backgroundColor = [[UIColor blackColor] CGColor];
+        innerClockFrameTicks.anchorPoint = CGPointMake(0, 0);
+        innerClockFrameTicks.position = CGPointMake(0 ,size/2 - secondsLength);
+        innerClockFrameTicks.bounds = CGRectMake(0, 0, secondsWidth, secondsLength);
+        
+        [innerClockFrame addSublayer:innerClockFrameTicks];
+        innerClockFrame.transform = CATransform3DMakeRotation(i * ([self convertDegreesToRadians:degreeForOneThirdSecond]), 0, 0, 1);
         [clockFrame addSublayer:innerClockFrame];
     }
     return clockFrame;
+}
+
+- (CGFloat) convertDegreesToRadians:(CGFloat) degrees {
+    return degrees * M_PI/180.0;
 }
 
 - (void)viewDidLoad {
@@ -61,18 +82,18 @@
     CALayer *layer = [CALayer layer];
     UIView* view2 = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 300.0f, 300.0f)];
     layer.frame = [view2 frame];
-    layer.sublayerTransform = CATransform3DMakeScale(1, -1, 1);
+//    layer.sublayerTransform = CATransform3DMakeScale(1, -1, 1);
     [[view2 layer] addSublayer:layer];
     [self.view addSubview:view2];
     
     // Draw clock frame
-    [layer addSublayer:[self createSecondsForClockFrame:view2.bounds.size.height]];
+    [layer addSublayer:[self createTicksForClockFrame:view2.bounds.size.height]];
     
     
     //Draw the hours layer
     hourLayer = [CALayer layer];
     hourLayer.backgroundColor = [[UIColor blackColor] CGColor];
-    hourLayer.anchorPoint = CGPointMake(0.5, 0);
+    hourLayer.anchorPoint = CGPointMake(0.5, 1);
     hourLayer.position = CGPointMake(view2.bounds.size.width/2, view2.bounds.size.height/2);
     hourLayer.bounds = CGRectMake(0, 0, 5, view2.bounds.size.width/2 -100);
     [layer addSublayer:hourLayer];
@@ -80,7 +101,7 @@
     //Draw the minute layer
     minuteLayer = [CALayer layer];
     minuteLayer.backgroundColor = [[UIColor blueColor] CGColor];
-    minuteLayer.anchorPoint = CGPointMake(0.5, 0);
+    minuteLayer.anchorPoint = CGPointMake(0.5, 1);
     minuteLayer.position = CGPointMake(view2.bounds.size.width/2, view2.bounds.size.height/2);
     minuteLayer.bounds = CGRectMake(0, 0, 3, view2.bounds.size.width/2 -50);
     [layer addSublayer:minuteLayer];
@@ -88,9 +109,9 @@
     //Draw the seconds layer
     secondsLayer = [CALayer layer];
     secondsLayer.backgroundColor = [[UIColor redColor] CGColor];
-    secondsLayer.anchorPoint = CGPointMake(0, 0);
+    secondsLayer.anchorPoint = CGPointMake(0, 1);
     secondsLayer.position = CGPointMake(view2.bounds.size.width/2, view2.bounds.size.height/2);
-    secondsLayer.bounds = CGRectMake(0, 0, 2, view2.bounds.size.width/2 -20);
+    secondsLayer.bounds = CGRectMake(0, 0, 2, view2.bounds.size.width/2);
     [layer addSublayer:secondsLayer];
     
     //Place the hands at correct location
@@ -105,18 +126,13 @@
     CGFloat minuteAngle = minutes * (360/60);
     CGFloat secondsAngle = seconds * 360/60;
     
-    
-    hourLayer.transform = CATransform3DMakeRotation(hourAngle/180 *M_PI, 0, 0, 1);
-    minuteLayer.transform = CATransform3DMakeRotation(minuteAngle /180 *M_PI, 0, 0, 1);
-    secondsLayer.transform = CATransform3DMakeRotation(secondsAngle /180 *M_PI, 0, 0, 1);
-    
     CABasicAnimation *secondsAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
     secondsAnimation.repeatCount = HUGE_VALF;
     secondsAnimation.duration = 60;
     secondsAnimation.removedOnCompletion = NO;
     secondsAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    secondsAnimation.fromValue = @(secondsAngle * M_PI / 180);
-    secondsAnimation.byValue = @(-2 * M_PI);
+    secondsAnimation.fromValue = @([self convertDegreesToRadians:secondsAngle]);
+    secondsAnimation.byValue = @(2 * M_PI);
     [secondsLayer addAnimation:secondsAnimation forKey:@"SecondAnimationKey"];
     
     CABasicAnimation *minutesAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
@@ -124,8 +140,8 @@
     minutesAnimation.duration = 60*60;
     minutesAnimation.removedOnCompletion = NO;
     minutesAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    minutesAnimation.fromValue = @(-minuteAngle * M_PI / 180);
-    minutesAnimation.byValue = @(-2 * M_PI);
+    minutesAnimation.fromValue = @([self convertDegreesToRadians:minuteAngle]);
+    minutesAnimation.byValue = @(2 * M_PI);
     [minuteLayer addAnimation:minutesAnimation forKey:@"MinutesAnimationKey"];
     
     CABasicAnimation *hoursAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
@@ -133,8 +149,8 @@
     hoursAnimation.duration = 60*60*12;
     hoursAnimation.removedOnCompletion = NO;
     hoursAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    hoursAnimation.fromValue = @(-hourAngle * M_PI / 180);
-    hoursAnimation.byValue = @(-2 * M_PI);
+    hoursAnimation.fromValue = @([self convertDegreesToRadians:hourAngle]);
+    hoursAnimation.byValue = @(2 * M_PI);
     [hourLayer addAnimation:hoursAnimation forKey:@"HoursAnimationKey"];
 }
 
